02b7eaca473c76882ad660eddc084d8e412fd7b1
Prashant <prashant.lakshmanan@gmail.com>
2024-06-06 09:15:16 +0530

    Change in Demo_Test.java

--- Selenium_Maven/src/test/java/demopackage/Demo_Test.java
+++ Selenium_Maven/src/test/java/demopackage/Demo_Test.java
@@ -8,7 +8,7 @@ public class Demo_Test {
 	public void display() {
 		Reporter.log("First Demo Test",true);
 		Reporter.log("Changed in GitHub",true);
-		Reporter.log("Again changed in Eclipse",true);
+		Reporter.log("Addeded in Eclipse",true);
 	}
 
 }
